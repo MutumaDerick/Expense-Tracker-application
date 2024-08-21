6f7b0206d67e7d62378ee648b85408f66366b4c1
MutumaDerick <pdmutuma@gmail.com>
2024-08-21 04:05:05 +0300

    Added files

--- Backend/controllers/income.js
+++ Backend/controllers/income.js
@@ -1,28 +1,40 @@
-const IncomeSchema = require("../models/incomeModel");
+const IncomeSchema= require("../models/IncomeModel")
+
 
 exports.addIncome = async (req, res) => {
-   const {title, amount, category, description, date} = req.body
+    const {title, amount, category, description, date}  = req.body
 
-   const income = IncomeSchema({
+    const income = IncomeSchema({
         title,
         amount,
         category,
         description,
         date
-   })
+    })
 
-   try {
-    //validation
-        if(!title || !category || !description || !date) {
-        return res.status(400).json({message: 'All Fiels are required!'})
+    try {
+        //validations
+        if(!title || !category || !description || !date){
+            return res.status(400).json({message: 'All fields are required!'})
         }
-        if(amount <= 0 || !amount === 'number') {
-        return res.status(400).json({message: 'Amount must be a number'})
+        if(amount <= 0 || !amount === 'number'){
+            return res.status(400).json({message: 'Amount must be a positive number!'})
         }
         await income.save()
         res.status(200).json({message: 'Income Added'})
-    }catch (error) {
-    res.status(500).json({message: 'Server Error'})
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
     }
-   console.log(income)
+
+    console.log(income)
 }
+
+exports.getIncomes = async (req, res) => {
+    try {
+        const incomes = await IncomeSchema.find().sort({createdAt: -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server Error'})
+    }
+}
+
